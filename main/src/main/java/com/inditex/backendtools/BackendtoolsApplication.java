1a6d89806503f3c1a969f92623023c3e98edeefb
MBDani <dani.mb999@gmail.com>
2026-04-09 20:31:22 +0200

    chore: initialize project with hexagonal architecture

---
+++ main/src/main/java/com/inditex/backendtools/BackendtoolsApplication.java
@@ -0,0 +1,12 @@
+package com.inditex.backendtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendtoolsApplication.class, args);
+    }
+}
